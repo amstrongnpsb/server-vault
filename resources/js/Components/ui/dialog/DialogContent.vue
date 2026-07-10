3b92741e31b5769aaf8c4803acb98d8941336ee3
amstrongnpsb <amstrongpsb@gmail.com>
2026-07-10 18:49:51 +0700

    bugfix: fix border color

--- resources/js/Components/ui/dialog/DialogContent.vue
+++ resources/js/Components/ui/dialog/DialogContent.vue
@@ -2,38 +2,38 @@
 import { XIcon } from "@lucide/vue";
 import { reactiveOmit } from "@vueuse/core";
 import {
-  DialogClose,
-  DialogContent,
-  DialogPortal,
-  useForwardPropsEmits,
+    DialogClose,
+    DialogContent,
+    DialogPortal,
+    useForwardPropsEmits,
 } from "reka-ui";
 import { cn } from "@/lib/utils";
-import { Button } from '@/Components/ui/button';
+import { Button } from "@/Components/ui/button";
 import DialogOverlay from "./DialogOverlay.vue";
 
 defineOptions({
-  inheritAttrs: false,
+    inheritAttrs: false,
 });
 
 const props = defineProps({
-  forceMount: { type: Boolean, required: false },
-  disableOutsidePointerEvents: { type: Boolean, required: false },
-  asChild: { type: Boolean, required: false },
-  as: { type: null, required: false },
-  class: {
-    type: [Boolean, null, String, Object, Array],
-    required: false,
-    skipCheck: true,
-  },
-  showCloseButton: { type: Boolean, required: false, default: true },
+    forceMount: { type: Boolean, required: false },
+    disableOutsidePointerEvents: { type: Boolean, required: false },
+    asChild: { type: Boolean, required: false },
+    as: { type: null, required: false },
+    class: {
+        type: [Boolean, null, String, Object, Array],
+        required: false,
+        skipCheck: true,
+    },
+    showCloseButton: { type: Boolean, required: false, default: true },
 });
 const emits = defineEmits([
-  "escapeKeyDown",
-  "pointerDownOutside",
-  "focusOutside",
-  "interactOutside",
-  "openAutoFocus",
-  "closeAutoFocus",
+    "escapeKeyDown",
+    "pointerDownOutside",
+    "focusOutside",
+    "interactOutside",
+    "openAutoFocus",
+    "closeAutoFocus",
 ]);
 
 const delegatedProps = reactiveOmit(props, "class");
@@ -42,26 +42,34 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits);
 </script>
 
 <template>
-  <DialogPortal>
-    <DialogOverlay />
-    <DialogContent
-      data-slot="dialog-content"
-      v-bind="{ ...$attrs, ...forwarded }"
-      :class="
-        cn(
-          'bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-top-[2%] data-[state=open]:slide-in-from-top-[2%] ring-foreground/10 grid max-w-[calc(100%-2rem)] gap-4 rounded-xl p-4 text-sm ring-1 duration-200 ease-out sm:max-w-sm fixed top-1/2 left-1/2 z-50 w-full -translate-x-1/2 -translate-y-1/2 outline-none',
-          props.class,
-        )
-      "
-    >
-      <slot />
+    <DialogPortal>
+        <DialogOverlay />
+        <DialogContent
+            data-slot="dialog-content"
+            v-bind="{ ...$attrs, ...forwarded }"
+            :class="
+                cn(
+                    'bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-top-[2%] data-[state=open]:slide-in-from-top-[2%] ring-border grid max-w-[calc(100%-2rem)] gap-4 rounded-xl p-4 text-sm ring-1 duration-200 ease-out sm:max-w-sm fixed top-1/2 left-1/2 z-50 w-full -translate-x-1/2 -translate-y-1/2 outline-none',
+                    props.class,
+                )
+            "
+        >
+            <slot />
 
-      <DialogClose v-if="showCloseButton" data-slot="dialog-close" as-child>
-        <Button variant="ghost" class="absolute top-2 right-2" size="icon-sm">
-          <XIcon />
-          <span class="sr-only">Close</span>
-        </Button>
-      </DialogClose>
-    </DialogContent>
-  </DialogPortal>
+            <DialogClose
+                v-if="showCloseButton"
+                data-slot="dialog-close"
+                as-child
+            >
+                <Button
+                    variant="ghost"
+                    class="absolute top-2 right-2"
+                    size="icon-sm"
+                >
+                    <XIcon />
+                    <span class="sr-only">Close</span>
+                </Button>
+            </DialogClose>
+        </DialogContent>
+    </DialogPortal>
 </template>
